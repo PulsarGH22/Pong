a84c49ad9fb1b6293c313d824681ad0816134376
Pulsar <121718481+PulsarGH22@users.noreply.github.com>
2024-10-31 22:06:57 +0400

    Update game.py

--- game.py
+++ game.py
@@ -7,7 +7,10 @@ from random import randint
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+
+WIDTH = 1280
+HEIGHT = 720
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 running = True
 dt = 0
@@ -24,8 +27,52 @@ rightTurn = True
 
 playerPos = pygame.Vector2(10, 320)
 
-leftRect = pygame.Rect(10, 320, 30, 100)
-rightRect = pygame.Rect(1240, 320, 30, 100)
+wall_thickness = 10
+
+#creating objects
+paddle = pygame.image.load("paddle.png").convert_alpha()
+ball = pygame.image.load("ball.png").convert_alpha()
+leftPaddle_rect = paddle.get_rect()
+ball_rect = ball.get_rect()
+paddle_mask = pygame.mask.from_surface(paddle)
+ball_mask = pygame.mask.from_surface(ball)
+
+
+#top and bottom walls
+def draw_walls():
+    top = pygame.draw.line(screen,'white',(0,0),(WIDTH,0),wall_thickness)
+    bottom = pygame.draw.line(screen,'white',(0,HEIGHT),(WIDTH,HEIGHT),wall_thickness)
+    wall_list = [top,bottom]
+    return wall_list
+
+class Ball:
+    def __init__(self, position, radius, color, x_speed,y_speed):
+        self.position=position
+        self.radius=radius
+        self.color=color
+        self.x_speed=x_speed
+        self.y_speed=y_speed
+        self.circle =''
+
+    def draw(self):
+        self.circle = pygame.draw.circle(screen, self.color,self.position,self.radius)
+
+class Paddle:
+    def __init__(self, x_pos, y_pos, color, y_speed):
+        self.x_pos=x_pos
+        self.y_pos=y_pos
+        self.color=color
+        self.y_speed=y_speed
+        self.paddle =''
+
+    def draw(self):
+        self.paddle = screen.blit(paddle, (self.x_pos,self.y_pos))
+
+ball = Ball(ball_pos,10,"white",1,0)
+newleftPaddle = Paddle(screen.get_width() - 50, screen.get_height() / 2, "white",0)
+newrightPaddle = Paddle(50, screen.get_height() / 2, "white",0)
+
+draw_walls()
 
 while running:
     # poll for events
@@ -36,34 +83,45 @@ while running:
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
+    
 
-    pygame.draw.rect(screen, "white", leftRect)
-    pygame.draw.rect(screen, "white", rightRect)
-    circle = pygame.draw.circle(screen, "white", ball_pos, 10)
+    # pygame.draw.rect(screen, "white", leftRect)
+    # pygame.draw.rect(screen, "white", rightRect)
+    # circle = pygame.draw.circle(screen, "white", ball_pos, 10)
+
+    ball.draw()
+    newleftPaddle.draw()
+    newrightPaddle.draw()
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        leftRect.y -= 300 * dt
+        newleftPaddle.y_pos -= 300 * dt
     if keys[pygame.K_s]:
-        leftRect.y += 300 * dt
+        newleftPaddle.y_pos += 300 * dt
 
-    if rightTurn:
-        ball_pos[0] += 5
-    elif leftTurn:
-        ball_pos[0] -= 5
 
-    collideRight = rightRect.colliderect(circle)
-    collideLeft = leftRect.colliderect(circle)
+    #collision detection
+    if (ball.position.x == newrightPaddle.x_pos) or (ball.position.x == newleftPaddle.x_pos):
+        ball.position *=-1
 
-    if collideRight:
-        leftTurn = True
-        rightTurn = False
 
-    elif collideLeft:
-        leftTurn = False
-        rightTurn = True
+    # if rightTurn:
+    #     ball_pos[0] += 5
+    # elif leftTurn:
+    #     ball_pos[0] -= 5
 
-    pygame.draw.circle(screen, "white", ball_pos, 10)
+    # collideRight = rightRect.colliderect(circle)
+    # collideLeft = leftRect.colliderect(circle)
+
+    # if collideRight:
+    #     leftTurn = True
+    #     rightTurn = False
+
+    # elif collideLeft:
+    #     leftTurn = False
+    #     rightTurn = True
+
+    # pygame.draw.circle(screen, "white", ball_pos, 10)
     pygame.display.update()
 
     if firstRun == True:
